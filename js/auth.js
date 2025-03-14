9d3b17269b1ac0957742202dd3323a2ddb7ec203
Ghulam Mustafa <157616503+G-Mustafa1@users.noreply.github.com>
2025-03-15 03:02:18 +0500

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,30 +1,34 @@
 import { auth, onAuthStateChanged, signOut } from "./firebase-Config.js";
 
 onAuthStateChanged(auth, (user) => {
-   if (user) {
-     console.log("User logged in:", user.email);
- 
-     if (user.email === "admin@example.com") {
-       window.location.href = "./admin.html";
-     }
-   } else {
-     console.log("User logged out");
- 
-     if (!window.location.href.includes("./login.html")) {
-       window.location.href = "./login.html";
-     }
-   }
- });
+  if (user) {
+    console.log("User logged in:", user.email);
+
+    if (user.email === "admin@example.com") {
+      window.location.href = "./admin.html";
+    }
+  } else {
+    console.log("User logged out");
+
+    if (!window.location.href.includes("./login.html")) {
+      window.location.href = "./login.html";
+    }
+  }
+});
 
 const logoutBtn = document.getElementById("logoutBtn")
 logoutBtn.addEventListener("click", () => {
-   signOut(auth)
-     .then(() => {
-       console.log("User logged out");
-       window.location.href = "./login.html";
-     })
-     .catch((error) => {
-       console.error("Logout error:", error);
-     });
- });
- 
+  signOut(auth)
+  Swal.fire({
+    title: "Log Out",
+    text: "Logout successfully!",
+    icon: "success"
+  })
+    .then(() => {
+      console.log("User logged out");
+      window.location.href = "./login.html";
+    })
+    .catch((error) => {
+      console.error("Logout error:", error);
+    });
+});
